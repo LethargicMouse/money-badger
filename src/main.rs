ff3526c413edb122891d655f381584f51f8b6740
George Kozirev <gkozirev.0@gmail.com>
2026-07-23 03:18:51 +0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
+use ratatui::{DefaultTerminal, Frame, crossterm::event};
+
 fn main() {
-    println!("Hello, world!");
+    ratatui::run(run_app).unwrap()
+}
+
+fn run_app(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
